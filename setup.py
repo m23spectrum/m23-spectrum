bdaf0208c09d83769e086a94a910e1c0ade3dcee
m23spectrum <m23_spectrum@proton.me>
2026-02-02 02:46:00 +0300

    Fix setup.py: Change README_M23.md to README.md
    Resolves ModuleNotFoundError when installing package with pip install -e .
    The file README_M23.md does not exist in the repository.
    Setup.py should reference the correct README.md file instead.

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ Setup script for M23-Spectrum package
 
 from setuptools import setup, find_packages
 
-with open("README_M23.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+with open("README.md", "r", encoding="utf-8") as fh:    long_description = fh.read()
 
 setup(
     name="m23-spectrum",
